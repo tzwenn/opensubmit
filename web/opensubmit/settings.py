f436efa0b4d5084c1edf9d41905e78a9547710cb
Sven Koehler <sven.koehler@hpi.de>
2019-01-17 16:13:58 +0100

    Implement simple social pipeline entry to merge OpenId and OIDC users

--- web/opensubmit/settings.py
+++ web/opensubmit/settings.py
@@ -427,6 +427,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.social_auth.associate_by_email',  # Transition for existing users
+    'opensubmit.social.oidc.associate_with_open_id',
     'social_core.pipeline.user.create_user',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
--- web/opensubmit/social/oidc.py
+++ web/opensubmit/social/oidc.py
@@ -5,6 +5,8 @@ This is our adjustment to the OpenID Connect provider from Python Social.
 from django.conf import settings
 from social_core.backends.open_id_connect import OpenIdConnectAuth as OpenIdConnectAuthBase
 
+from social_core.backends.open_id import OpenIdAuth
+
 class OpenIdConnectAuth(OpenIdConnectAuthBase):
 	OIDC_ENDPOINT = getattr(settings, "LOGIN_OIDC_ENDPOINT", None)
 
@@ -14,3 +16,30 @@ class OpenIdConnectAuth(OpenIdConnectAuthBase):
 
 	def get_key_and_secret(self):
 		return settings.LOGIN_OIDC_CLIENT_ID, settings.LOGIN_OIDC_CLIENT_SECRET
+
+def associate_with_open_id(backend, details, user=None, *args, **kwargs):
+	"""
+	Associate the current auth with an OpenID auth in the pattern of
+
+		https://openid.provider.org/user/<uid>
+	
+	where <uid> is the user id as served oy OpenID Connnect.
+
+	This pipeline entry will fail if the old OpenID provider uses another
+	pattern, or somehow userids got altered. (Technical limitations like
+	maximum length in dicrectory services, name changes by marriage, etc.)
+	"""
+
+	if user or not kwargs.get("uid"):
+		return None
+	
+	uid = kwargs["uid"]
+	oid_uid = "/".join([settings.OPENID_PROVIDER, "user", uid]) 
+	oid_social_auth = backend.strategy.storage.user.objects.get_social_auth(OpenIdAuth.name, oid_uid)
+
+	if oid_social_auth is None:
+		return None
+	
+	return {'user': oid_social_auth.user,
+			'is_new': False}
+
